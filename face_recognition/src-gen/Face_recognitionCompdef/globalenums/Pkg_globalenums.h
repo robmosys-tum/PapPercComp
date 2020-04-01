44365622d8fb23ce9fa7560281cd096d12a943ff
Bare Luka Zagar <bare.luka.zagar@gmail.com>
2020-04-01 08:04:00 +0200

    Face_Detection component

---
+++ face_recognition/src-gen/Face_recognitionCompdef/globalenums/Pkg_globalenums.h
@@ -0,0 +1,33 @@
+#ifndef PKG_FACE_RECOGNITIONCOMPDEF_GLOBALENUMS
+#define PKG_FACE_RECOGNITIONCOMPDEF_GLOBALENUMS
+
+/************************************************************
+ Pkg_globalenums package header
+ ************************************************************/
+
+#include "Face_recognitionCompdef/Pkg_Face_recognitionCompdef.h"
+
+#ifndef _IN_
+#define _IN_
+#endif
+#ifndef _OUT_
+#define _OUT_
+#endif
+#ifndef _INOUT_
+#define _INOUT_
+#endif
+
+/* Package dependency header include                        */
+
+namespace Face_recognitionCompdef {
+namespace globalenums {
+
+// Types defined within the package
+}// of namespace globalenums
+} // of namespace Face_recognitionCompdef
+
+/************************************************************
+ End of Pkg_globalenums package header
+ ************************************************************/
+
+#endif
