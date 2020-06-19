faf90677a68c8238dd93c5aaa342c30a44664959
Ethan Tatlow <ethan.tatlow@me.com>
2020-06-19 08:51:26 +0200

    util: fix minor issues with model downlaod script

--- Kitchen Utensil Classifier/scripts/download_model.sh
+++ Kitchen Utensil Classifier/scripts/download_model.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-FILE_ID="1--GlyeQ8ALd2rr6JDx3XFJt_67mooH78"
-TARGET_FILE="../data/kitchen_classifier.onnx"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+FILE_ID="1pWX9lu46fGFOr3TgtWKgyDT9NgsbV-UN"
+TARGET_FILE="${SCRIPT_DIR}/../data/kitchen_classifier.onnx"
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1--GlyeQ8ALd2rr6JDx3XFJt_67mooH78' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=${FILE_ID}" -O ${TARGET_FILE} && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id='${FILE_ID} -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=${FILE_ID}" -O ${TARGET_FILE} && rm -rf /tmp/cookies.txt
