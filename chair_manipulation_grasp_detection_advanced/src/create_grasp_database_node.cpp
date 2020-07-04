0d7e7b96b9b7c895914fc84462b4ade93af81a73
Philipp Badenhoop <philipp.badenhoop@hellocodeblog.com>
2020-07-04 10:01:04 +0200

    Create advanced grasp detection package skeleton

---
+++ chair_manipulation_grasp_detection_advanced/src/create_grasp_database_node.cpp
@@ -0,0 +1,7 @@
+#include <ros/ros.h>
+
+int main(int argc, char *argv[])
+{
+  ros::init(argc, argv, "create_grasp_database_node");
+  return 0;
+}
---
+++ chair_manipulation_grasp_detection_advanced/src/grasp_detection_node.cpp
@@ -0,0 +1,7 @@
+#include <ros/ros.h>
+
+int main(int argc, char *argv[])
+{
+  ros::init(argc, argv, "grasp_detection_node");
+  return 0;
+}
