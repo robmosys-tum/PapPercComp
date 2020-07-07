4438ed31dbf0c5a9306c9d1fb03aa19b88c34780
Philipp Seitz <philipp.seitz@tum.de>
2020-07-07 09:44:50 +0200

    update disease_service to new msg/srv

--- scripts/disease_service.py
+++ scripts/disease_service.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
+import cv2
 import numpy as np
 import rclpy
 import tensorflow as tf
+from cv_bridge import CvBridge
 from rclpy.node import Node
 
 from fruit_detection.srv import Classification
@@ -15,35 +17,45 @@ class DiseaseService(Node):
         self.get_logger().info('Loading Model')
         self.model = tf.keras.models.load_model(
             'src/fruit_detection/networks/squeezenet.h5')
+        self.bridge = CvBridge()
+        self.width = 256
+        self.height = 256
         self.diseases = ['healthy', 'rotten']
         self.srv = self.create_service(Classification, 'DiseaseService',
                                        self.predict)
         self.get_logger().info('Service Started')
 
     def predict(self, request, response):
-        self.get_logger().info('Incoming request: %s' % (request.file))
-        image = self.decode_img_path(request.file, 256, 256)
-
-        prediction = self.model.predict([image], batch_size=1)
-        response.disease = self.diseases[np.argmax(prediction)]
-        self.get_logger().info('Sent Response %s' % response.disease)
+        self.get_logger().info('Incoming request, starting classification')
+        image = self.decode_request(request)
+        predictions = self.model.predict(image)
+        self.get_logger().info('Classification complete')
+        new_boxes = request.boxes
+        for index, box in enumerate(new_boxes):
+            box.disease = self.diseases[np.argmax(predictions[index])]
+        response.new_boxes = new_boxes
+        self.get_logger().info('Sending response %s' % new_boxes)
         return response
 
-    def decode_img(self, img, width, height):
-        # TODO test
-        img = tf.image.decode_raw(img, tf.float32)
-        img = img / 255.0
-        img = tf.image.resize(img, [width, height])
-        img = tf.expand_dims(img, 0)
-        return img
-
-    def decode_img_path(self, filename, width, height):
-        img = tf.io.read_file(filename)
-        img = tf.image.decode_image(img, channels=3, expand_animations=False)
+    def decode_request(self, request):
+        cv_image = self.bridge.imgmsg_to_cv2(request.img, "bgr8")
+        image_data = cv2.imencode('.jpg', cv_image)[1].tostring()
+        img = tf.image.decode_jpeg(image_data, channels=3)
         img = tf.cast(img, tf.float32) / 255.0
         img = tf.image.convert_image_dtype(img, tf.float32)
-        img = tf.image.resize(img, [width, height])
         img = tf.expand_dims(img, 0)
+        bounding_boxes = []
+        box_indices = np.zeros(len(request.boxes))
+        for box in request.boxes:
+            bounding_boxes.append([box.ymin, box.xmin, box.ymax, box.xmax])
+        bounding_boxes = tf.convert_to_tensor(bounding_boxes, tf.float32)
+        box_indices = tf.convert_to_tensor(box_indices, tf.int32)
+        crop = tf.convert_to_tensor([self.width, self.height], tf.int32)
+        img = tf.image.crop_and_resize(img,
+                                       bounding_boxes,
+                                       box_indices,
+                                       crop,
+                                       extrapolation_value=0.)
         return img
 
 
