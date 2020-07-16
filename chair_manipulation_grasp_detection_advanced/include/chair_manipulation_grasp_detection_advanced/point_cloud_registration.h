b397bdc91d7685635b0109bac4a5b1ab8f470e89
Philipp Badenhoop <philipp.badenhoop@hellocodeblog.com>
2020-07-16 14:48:24 +0200

    Add custom cpd repo

--- chair_manipulation_grasp_detection_advanced/include/chair_manipulation_grasp_detection_advanced/point_cloud_registration.h
+++ chair_manipulation_grasp_detection_advanced/include/chair_manipulation_grasp_detection_advanced/point_cloud_registration.h
@@ -1,8 +1,45 @@
-//
-// Created by philipp on 15.07.20.
-//
-
 #ifndef CHAIR_MANIPULATION_GRASP_DETECTION_ADVANCED_POINT_CLOUD_REGISTRATION_H
 #define CHAIR_MANIPULATION_GRASP_DETECTION_ADVANCED_POINT_CLOUD_REGISTRATION_H
+
+#include <ros/ros.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <cpd/nonrigid.hpp>
+
+namespace chair_manipulation
+{
+struct PointCloudRegistrationParameters
+{
+  void load(ros::NodeHandle& nh);
+
+  double lambda_;
+  double beta_;
+  int max_iterations_;
+};
+
+class PointCloudRegistration
+{
+public:
+  using PointT = pcl::PointNormal;
+  using PointCloud = pcl::PointCloud<PointT>;
+  using PointCloudPtr = PointCloud::Ptr;
+  using PointCloudConstPtr = PointCloud::ConstPtr;
+
+  explicit PointCloudRegistration(PointCloudRegistrationParameters params);
+
+  void setInputSource(const PointCloudConstPtr& source_cloud);
+
+  void setInputTarget(const PointCloudConstPtr& target_cloud);
+
+  void align(PointCloud& aligned_cloud);
+
+private:
+  PointCloudRegistrationParameters params_;
+  PointCloudConstPtr source_cloud_;
+  PointCloudConstPtr target_cloud_;
+  cpd::Nonrigid nonrigid_;
+};
+
+}
 
 #endif  // CHAIR_MANIPULATION_GRASP_DETECTION_ADVANCED_POINT_CLOUD_REGISTRATION_H
--- chair_manipulation_grasp_detection_advanced/src/point_cloud_registration.cpp
+++ chair_manipulation_grasp_detection_advanced/src/point_cloud_registration.cpp
@@ -1,3 +1,31 @@
-//
-// Created by philipp on 15.07.20.
-//
+#include <chair_manipulation_grasp_detection_advanced/point_cloud_registration.h>
+
+namespace chair_manipulation
+{
+void PointCloudRegistrationParameters::load(ros::NodeHandle& nh)
+{
+  lambda_ = nh.param<double>("lambda", cpd::DEFAULT_LAMBDA);
+  beta_ = nh.param<double>("beta", cpd::DEFAULT_BETA);
+  max_iterations_ = nh.param<int>("max_iterations", cpd::DEFAULT_MAX_ITERATIONS);
+}
+
+PointCloudRegistration::PointCloudRegistration(PointCloudRegistrationParameters params) : params_(std::move(params))
+{
+  nonrigid_.lambda(params_.lambda_);
+  nonrigid_.beta((params.beta_));
+  nonrigid_.max_iterations(params.max_iterations_);
+  nonrigid_.linked(true);
+  nonrigid_.normalize(true);
+}
+
+void PointCloudRegistration::setInputSource(const PointCloudRegistration::PointCloudConstPtr& source_cloud)
+{
+  source_cloud_ = source_cloud;
+}
+
+void PointCloudRegistration::setInputTarget(const PointCloudRegistration::PointCloudConstPtr& target_cloud)
+{
+  target_cloud_ = target_cloud;
+}
+
+}  // namespace chair_manipulation
