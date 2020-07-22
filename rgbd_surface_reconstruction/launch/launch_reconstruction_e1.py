e3d44798ac4b49e551dab12b2c6a875e49a157b8
DanielHettegger <daniel.hettegger@tum.de>
2020-07-22 16:43:49 +0200

    Updated to new fancy launch files!

--- rgbd_surface_reconstruction/launch/launch_reconstruction_e1.py
+++ rgbd_surface_reconstruction/launch/launch_reconstruction_e1.py
@@ -1,16 +1,49 @@
-
-
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+import os
+from zipfile import ZipFile
+import urllib
+import signal
+
+def signal_handler(sig, frame):
+    quit()
+
+original_sigint_handler = signal.getsignal(signal.SIGINT)
+signal.signal(signal.SIGINT, signal_handler)
+
+def download_dataset(corbs_datasetpath, dataset_url):
+    corbs_zip_path = corbs_datasetpath+'/zipfolder.zip'
+    urllib.request.urlretrieve(dataset_url, corbs_zip_path)
+    with ZipFile(corbs_zip_path, 'r') as zip_ref:
+        zip_ref.extractall(corbs_datasetpath)
+    os.remove(corbs_zip_path)
+
 def generate_launch_description():
+    corbs_datasetpath = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '../', 'datasets'))
+    
+    download_indicator = corbs_datasetpath + '/E1_T'
+    dataset_url = "http://corbs.dfki.uni-kl.de/suppfiles/ElectricalCabinet/E1/E1_Trajectory.zip"
+    if not os.path.isfile(download_indicator):
+        download_dataset(corbs_datasetpath, dataset_url)
+        open(download_indicator, 'a').close()
+
+    download_indicator = corbs_datasetpath + '/E1_D'
+    dataset_url = "http://corbs.dfki.uni-kl.de/suppfiles/ElectricalCabinet/E1/E1_pre_registereddata.zip"
+    if not os.path.isfile(download_indicator):
+        print("Dataset not found!")
+        print("Downloading dataset please wait... (This will take a few minutes)")
+        download_dataset(corbs_datasetpath, dataset_url)
+        open(download_indicator, 'a').close()
+
+    signal.signal(signal.SIGINT, original_sigint_handler)
     return LaunchDescription([
         Node(
             package='ros2_corbs_publisher',
             node_executable='ros2_corbs_publisher_node',
             parameters=[
-                {"data_path": "/home/drkmtr/Downloads/CORBS/E1_pre_registereddata/",
-                 "trajectory_path": "/home/drkmtr/Downloads/CORBS/E1_Trajectory/"}],
+                {"data_path": corbs_datasetpath+"/E1_pre_registereddata/",
+                 "trajectory_path": corbs_datasetpath+"/E1_Trajectory/"}],
             output='screen',
             emulate_tty=True,
         ),
