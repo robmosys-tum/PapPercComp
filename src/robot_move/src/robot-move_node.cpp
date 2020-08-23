619ef51f0b1d9cc3c8c5c80f769ee99f15e2b54f
Omar <moar.ahmed@gmail.com>
2020-08-23 12:09:43 +0200

    comments and code cleanup

--- src/robot_move/src/robot-move_node.cpp
+++ src/robot_move/src/robot-move_node.cpp
@@ -1,36 +1,33 @@
 #include <ros/ros.h>
 
+//MoveIt imports
 #include <moveit/robot_model_loader/robot_model_loader.h>
 #include <moveit/planning_interface/planning_interface.h>
 #include <moveit/move_group_interface/move_group_interface.h>
-
 #include <moveit/planning_scene/planning_scene.h>
 #include <moveit/robot_model/robot_model.h>
 #include <moveit/robot_state/robot_state.h>
 #include <moveit/kinematic_constraints/utils.h>
 #include <moveit_msgs/DisplayTrajectory.h>
-
 #include <moveit_visual_tools/moveit_visual_tools.h>
+
 #include <boost/scoped_ptr.hpp>
-#include "std_msgs/String.h"
+
+//classbox message header file
 #include "classbox.h"
 
-
-ros::NodeHandle* handle;
-
+// coordinates struct to save the center of the published classbox
 struct Coordinates {
     float x;
     float y;
 };
 
 /**
- * This tutorial demonstrates simple receipt of messages over the ROS system.
+ * Subscriber callback function
  *
- */
-
+*/
 int chatterCallback(const robot_move::classbox classbox ) {
-
-    if (classbox.disease == "undefined" || classbox.disease_score < 3) {
+    if (classbox.disease == "undefined" || classbox.disease_score < 3) { // if no fruit disease or classification score is too low.
         ROS_INFO("no disease or low score");
         ROS_INFO("score %i", classbox.disease_score);
         return 0;
@@ -49,16 +46,16 @@ int chatterCallback(const robot_move::classbox classbox ) {
 
     const std::string PLANNING_GROUP = "arm";
 
-    moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
+    moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP); //initalize arm move grou
 
+    // load urdf model (data injected from launch file)
     robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
     robot_model::RobotModelPtr robot_model = robot_model_loader.getModel();
 
     ROS_INFO("Model frame: %s", robot_model->getModelFrame().c_str());
 
-    moveit::core::RobotStatePtr robot_state(new moveit::core::RobotState(robot_model));
-    const moveit::core::JointModelGroup* joint_model_group = robot_state->getJointModelGroup(PLANNING_GROUP);
-
+    moveit::core::RobotStatePtr robot_state(new moveit::core::RobotState(robot_model)); //get start statrt for robot
+    const moveit::core::JointModelGroup* joint_model_group = robot_state->getJointModelGroup(PLANNING_GROUP); //initalize joint model group with planning group (currenlty it is the arm)
 
     planning_scene::PlanningScenePtr planning_scene(new planning_scene::PlanningScene(robot_model));
 
@@ -71,24 +68,20 @@ int chatterCallback(const robot_move::classbox classbox ) {
 
     if (!node_handle.getParam("planning_plugin", planner_plugin_name))
         ROS_FATAL_STREAM("Could not find planner plugin name");
-    try
-    {
+    try {
         planner_plugin_loader.reset(new pluginlib::ClassLoader<planning_interface::PlannerManager>(
                 "moveit_core", "planning_interface::PlannerManager"));
     }
-    catch (pluginlib::PluginlibException& ex)
-    {
+    catch (pluginlib::PluginlibException& ex) {
         ROS_FATAL_STREAM("Exception while creating planning plugin loader " << ex.what());
     }
-    try
-    {
+    try {
         planner_instance.reset(planner_plugin_loader->createUnmanagedInstance(planner_plugin_name));
         if (!planner_instance->initialize(robot_model, node_handle.getNamespace()))
             ROS_FATAL_STREAM("Could not initialize planner instance");
         ROS_INFO_STREAM("Using planning interface '" << planner_instance->getDescription() << "'");
     }
-    catch (pluginlib::PluginlibException& ex)
-    {
+    catch (pluginlib::PluginlibException& ex) {
         const std::vector<std::string>& classes = planner_plugin_loader->getDeclaredClasses();
         std::stringstream ss;
         for (std::size_t i = 0; i < classes.size(); ++i)
@@ -116,8 +109,10 @@ via buttons and keyboard shortcuts in RViz */
 /* Batch publishing is used to reduce the number of messages being sent to RViz for large visualizations */
     visual_tools.trigger();
 
+//    plann interface object
     moveit::planning_interface::MoveGroupInterface::Plan my_plan;
 
+//  pose object to set the new position for the end-effector
     geometry_msgs::PoseStamped pose;
 
     planning_interface::MotionPlanRequest req;
@@ -126,34 +121,37 @@ via buttons and keyboard shortcuts in RViz */
     geometry_msgs::PoseStamped orig_pose =  move_group.getCurrentPose();
     ROS_INFO("value %s", move_group.getEndEffectorLink().c_str());
     pose = orig_pose;
+
+//  set the coordinates of the end-effector
     pose.pose.position.x = coordinates.x;
     pose.pose.position.y = coordinates.y;
     pose.pose.position.z = 0.2;
 
-
+//  create pose goal on link 6 (link connecting the end-effector)
     moveit_msgs::Constraints pose_goal =
             kinematic_constraints::constructGoalConstraints("link_6", pose);
 
     req.group_name = PLANNING_GROUP;
     req.goal_constraints.push_back(pose_goal);
 
-    planning_interface::PlanningContextPtr context =
+    planning_interface::PlanningContextPtr context = //create planning context ptr from the pose goal request
             planner_instance->getPlanningContext(planning_scene, req, res.error_code_);
-    context->solve(res);
-    if (res.error_code_.val != res.error_code_.SUCCESS)
-    {
+    context->solve(res); //solve the motion planning problem for the robot
+
+    if (res.error_code_.val != res.error_code_.SUCCESS) {
         ROS_ERROR("Could not compute plan successfully");
         return 0;
     }
 
     moveit_msgs::MotionPlanResponse response;
-    res.getMessage(response);
+    res.getMessage(response); //obtain motion planning response
 
+    //rotate joints according to the plan
     robot_state->setJointGroupPositions(joint_model_group, response.trajectory.joint_trajectory.points.back().positions);
+//  set the planning scene state to match the new robot state after joints' rotation
     planning_scene->setCurrentState(*robot_state.get());
+//  publish the new robot state to rviz
     visual_tools.publishRobotState(planning_scene->getCurrentStateNonConst(), rviz_visual_tools::GREEN);
-    visual_tools.publishAxisLabeled(pose.pose, "goal_1");
-    visual_tools.publishText(text_pose, "Pose Goal (1)", rvt::WHITE, rvt::XLARGE);
     visual_tools.trigger();
 
     return 0;
@@ -172,6 +170,7 @@ int main(int argc, char** argv) {
     ros::AsyncSpinner spinner(2);
     spinner.start();
 
+//  create a subscriber listening on the topic chatter
     ros::Subscriber sub = node_handle.subscribe("chatter", 1000, chatterCallback2);
 
     ros::waitForShutdown();
